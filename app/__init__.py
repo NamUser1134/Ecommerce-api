7db0ffb2584389e642ec87f17bc69b3de4fe71c8
Yamade <namdang367@gmail.comm>
2024-09-10 11:46:24 +0700

    thêm cau hinh cors cho phep

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from pymongo import MongoClient
+from flask_cors import CORS
 
 # Initialize Flask app
 app = Flask(__name__)
 
+
+# Enable CORS for specific routes and origins
+CORS(app, resources={r"/api/*": {"origins": ""}})
 # Kết nối tới MongoDB
 client = MongoClient("mongodb+srv://namdang3570:minhquy13@cluster0.eafdrjb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client['online_shop']
